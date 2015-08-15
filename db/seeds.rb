c013c6fa8b17cb5bcc3065f5dd2b54893c3049fe
Josh Arnold <joshuaarnold22@gmail.com>
2015-08-15 10:28:12 -0400

    Completed the Seed Checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 require 'faker'
+3.times do
+  user = User.new(
+  name: Faker::Name.name,
+  email: Faker::Internet.email,
+  password: Faker::Lorem.characters(8)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
 
 10.times do
   Wiki.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
